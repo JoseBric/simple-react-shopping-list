187e32c7c7d8d1139fce62aeade050b877931f4b
Jose Briceno <josebric@hotmail.com>
2019-06-09 22:09:55 -0700

    new settings

--- server.js
+++ server.js
@@ -12,17 +12,18 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json())
 app.use('/api/items', items)
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-}
 
 const db = process.env.MONGO_URI
 
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected...'))
   .catch((err) => console.log(err))
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
