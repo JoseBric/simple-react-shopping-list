537c16f3f0f95d91f282d9bcba8ae46fcb19dc39
JoseBric <josebric@hotmail.com>
2019-10-26 11:17:48 -0700

    bug fix

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const path = require('path')
 
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const express = require('express')
 const items = require('./routes/api/items')
 
 const app = express()
+
 const port = process.env.PORT || 5000;
+const db = process.env.MONGO_URI
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next()
+})
 
 app.use(bodyParser.json())
 app.use('/api/items', items)
-
-
-const db = process.env.MONGO_URI
 
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected...'))
@@ -28,6 +35,5 @@ if(process.env.NODE_ENV === 'production') {
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
 function closeApp(){process.exit(0)}
-
 process.on('uncaughtException', closeApp)
 process.on('SIGTERM', closeApp)
